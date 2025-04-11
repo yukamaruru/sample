9dc3811caf981792e1f2c8b90d1a47207462886a
yukamaru <yuun.vv07@gmail.com>
2025-04-11 21:51:42 +0900

    参照型の構造とメソッド作成の学習を追加

--- src/jp/co/diworks/action/Main.java
+++ src/jp/co/diworks/action/Main.java
@@ -4,5 +4,33 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println(gokei());
+		System.out.println(gokei2(2, 3));
+		System.out.println(gokei3(5, 7));
+		System.out.println(circle(5));
+		System.out.println(hikizan(10, 10));
+		System.out.println(kakezan(10, 10));
+		System.out.println(warizan(10, 10));
+	}
+	public static int gokei() {
+		return 1+1;
+	}
+	public static int gokei2(int number1, int number2) {
+		return number1+number2;
+	}
+	public static int gokei3(int number1,int number2) {
+		return number1+number2;
+	}
+	public static double circle(int hankei) {
+		return hankei*hankei*3.14;
+	}
+	public static int hikizan(int number1, int number2) {
+		return number1-number2;
+	}
+	public static int kakezan(int number1, int number2) {
+		return number1*number2;
+	}
+	public static int warizan(int number1, int number2) {
+		return number1/number2;
 	}
 }
